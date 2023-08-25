487de3b9cd25dc48a762892b865b644c8d5022f0
jcruz-2018059 <jcruz-2018059@kinal.edu.gt>
2023-08-25 12:28:47 -0600

    security module

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Third Parties
-import { LoginComponent } from './login/login.component';
 import { SecurityRoutingModule } from './security-routing.module';
 
 @NgModule({
